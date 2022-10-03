8ac8d82c1d2cd5e0fbec75622ab8a473b9e6e01d
Matt <97047080+mattxmade@users.noreply.github.com>
2022-10-03 13:08:48 +0100

    Update Calendar.js

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,9 +1,9 @@
-import Doc from './Doc';
+import Doc from "./Doc";
 
-import { getMonth } from 'date-fns';
-import { isLeapYear } from 'date-fns';
+import { getMonth } from "date-fns";
+import { isLeapYear } from "date-fns";
 
-let lastDay = '';
+let lastDay = "";
 
 function style(element, property, value) {
   element.style[property] = value;
@@ -11,20 +11,19 @@ function style(element, property, value) {
 
 function generateDays(days, parent) {
   for (let i = 1; i <= days; i++) {
+    let day = Doc("create", "div");
+    day.classList.add("day");
 
-    let day = Doc('create', 'div');
-    day.classList.add('day');
-
-    let name = Doc('create', 'p');
+    let name = Doc("create", "p");
     name.textContent = i;
 
     day.appendChild(name);
 
-    day.addEventListener('click', (e) => {
-      if (lastDay !== '') {
-        lastDay.style.backgroundColor = 'transparent';
+    day.addEventListener("click", (e) => {
+      if (lastDay !== "") {
+        lastDay.style.backgroundColor = "transparent";
       }
-      e.target.style.backgroundColor = 'lightgrey';
+      e.target.style.backgroundColor = "lightgrey";
       lastDay = e.target;
     });
 
@@ -33,80 +32,81 @@ function generateDays(days, parent) {
 }
 
 function Calendar() {
-
   // Calendar div container
-  const container = Doc('create', 'div');
-  container.classList.add('calendar');
+  const container = Doc("create", "div");
+  container.classList.add("calendar");
 
-  style(container, 'display', 'grid');
-  style(container, 'align-items', 'center');
+  style(container, "display", "grid");
+  style(container, "align-items", "center");
 
   const monthTabs = [];
 
   // 12 months :)
-  for(let i = 1; i <= 12; i++) {
+  for (let i = 1; i <= 12; i++) {
     let name;
-    
+
     // month div - holds day elements
-    let month = Doc('create', 'div');
-    month.classList.add('calendar-month');
-    
+    let month = Doc("create", "div");
+    month.classList.add("calendar-month");
+
     // month title
-    let h2 = Doc('create', 'h3');
+    let h2 = Doc("create", "h3");
 
     // days
-    let days = Doc('create', 'div');
-    days.classList.add('calendar-days');
+    let days = Doc("create", "div");
+    days.classList.add("calendar-days");
 
-    switch(i) {
+    switch (i) {
       case 1:
-        name = 'January';
+        name = "January";
         generateDays(31, days);
         break;
       case 2:
-        name = 'February';
-        isLeapYear( Date.now() ) ? generateDays(29, days) : generateDays(28, days);
+        name = "February";
+        isLeapYear(Date.now())
+          ? generateDays(29, days)
+          : generateDays(28, days);
         break;
       case 3:
-        name = 'March';
+        name = "March";
         generateDays(31, days);
         break;
       case 4:
-        name = 'April';
+        name = "April";
         generateDays(30, days);
         break;
       case 5:
-        name = 'May';
+        name = "May";
         generateDays(31, days);
         break;
       case 6:
-        name = 'June';
+        name = "June";
         generateDays(30, days);
         break;
       case 7:
-        name = 'July';
+        name = "July";
         generateDays(31, days);
         break;
       case 8:
-        name = 'August';
+        name = "August";
         generateDays(31, days);
         break;
       case 9:
-        name = 'September';
+        name = "September";
         generateDays(30, days);
         break;
       case 10:
-        name = 'October';
+        name = "October";
         generateDays(31, days);
         break;
       case 11:
-        name = 'November';
+        name = "November";
         generateDays(30, days);
         break;
       case 12:
-        name = 'December';
+        name = "December";
         generateDays(31, days);
-        break;     
+        break;
     }
 
     // title <p>
@@ -117,123 +117,116 @@ function Calendar() {
 
     month.id = i;
     month.classList.add(name);
-    month.classList.add('month')
+    month.classList.add("month");
     container.appendChild(month);
 
     // Current month view
-    if (Number(month.id) !== getMonth( Date.now() )+1 ) {
-      style(month, 'visibility', 'hidden');
+    if (Number(month.id) !== getMonth(Date.now()) + 1) {
+      style(month, "visibility", "hidden");
     }
 
     // month <div>
-    style(month, 'width', '90%');
-    style(month, 'height', '100%');
-    style(days, 'display', 'grid');
-    style(month, 'textAlign', 'center');
-    style(month, 'position', 'absolute');
-    style(month, 'backgroundColor', 'white');
-    style(month, 'justify-self', 'center');
-    style(days, 'grid-template-columns', 'repeat(7, 1fr)');
+    style(month, "width", "90%");
+    style(month, "height", "100%");
+    style(days, "display", "grid");
+    style(month, "textAlign", "center");
+    style(month, "position", "absolute");
+    style(month, "backgroundColor", "white");
+    style(month, "justify-self", "center");
+    style(days, "grid-template-columns", "repeat(7, 1fr)");
 
     monthTabs.push(month);
   }
-  style(container, 'position', 'relative');
+  style(container, "position", "relative");
 
-  const closeCal = Doc('create', 'button');
-  closeCal.type = 'button';
-  closeCal.classList.add('js-close-calendar-btn');
-  closeCal.textContent = 'close';
+  const closeCal = Doc("create", "button");
+  closeCal.type = "button";
+  closeCal.classList.add("js-close-calendar-btn");
+  closeCal.textContent = "close";
   container.appendChild(closeCal);
 
-  const navLeft = Doc('create', 'i');
-  navLeft.classList.add('fas');
-  navLeft.classList.add('fa-chevron-circle-left');
+  const navLeft = Doc("create", "i");
+  navLeft.classList.add("fas");
+  navLeft.classList.add("fa-chevron-circle-left");
 
-  const navRight = Doc('create', 'i');
-  navRight.classList.add('fas');
-  navRight.classList.add('fa-chevron-circle-right');
+  const navRight = Doc("create", "i");
+  navRight.classList.add("fas");
+  navRight.classList.add("fa-chevron-circle-right");
 
   container.appendChild(navLeft);
   container.appendChild(navRight);
 
-  style(navLeft, 'position', 'absolute');
-  style(navRight, 'position', 'absolute');
+  style(navLeft, "position", "absolute");
+  style(navRight, "position", "absolute");
 
-  style(navLeft, 'fontSize', '2rem');
-  style(navRight, 'fontSize', '2rem');
+  style(navLeft, "fontSize", "2rem");
+  style(navRight, "fontSize", "2rem");
 
-  style(navLeft, 'margin-left', '0.65rem');
-  style(navRight, 'margin-right', '0.65rem');
+  style(navLeft, "margin-left", "0.65rem");
+  style(navRight, "margin-right", "0.65rem");
 
-  navRight.dataset.index = getMonth( Date.now() )+1;
-  navRight.addEventListener('click', () => {
-    navRightHandler(navRight, navLeft, container)
+  navRight.dataset.index = getMonth(Date.now()) + 1;
+  navRight.addEventListener("click", () => {
+    navRightHandler(navRight, navLeft, container);
   });
 
-  navLeft.dataset.index = getMonth( Date.now() )+1;
-  navLeft.addEventListener('click', () => {
-    navLeftHandler(navLeft, navRight, container)
+  navLeft.dataset.index = getMonth(Date.now()) + 1;
+  navLeft.addEventListener("click", () => {
+    navLeftHandler(navLeft, navRight, container);
   });
 
-  return { container, navLeft, navRight, closeCal, monthTabs }
+  return { container, navLeft, navRight, closeCal, monthTabs };
 }
 
 function navRightHandler(nav, navAlt, container) {
   nav.dataset.index = Number(nav.dataset.index);
   navAlt.dataset.index = Number(navAlt.dataset.index);
-  
+
   nav.dataset.index++;
   navAlt.dataset.index++;
-  
+
   if (Number(nav.dataset.index) >= 13) {
-    nav.dataset.index = 1; 
-    navAlt.dataset.index  = 1;
+    nav.dataset.index = 1;
+    navAlt.dataset.index = 1;
   }
 
-  for(let month of container.children) {
+  for (let month of container.children) {
     //console.dir(month);
-  
-    if (month.tagName === 'DIV') {
-  
+
+    if (month.tagName === "DIV") {
       if (Number(nav.dataset.index) === Number(month.id)) {
-        month.style.visibility = 'visible';
-      } 
-      else { 
-        month.style.visibility = 'hidden';
+        month.style.visibility = "visible";
+      } else {
+        month.style.visibility = "hidden";
       }
     }
-  
-  }; 
+  }
 }
 
 function navLeftHandler(nav, navAlt, container) {
-  
   nav.dataset.index = Number(nav.dataset.index);
   navAlt.dataset.index = Number(navAlt.dataset.index);
-      
+
   nav.dataset.index--;
   navAlt.dataset.index--;
-  
+
   if (Number(nav.dataset.index) <= 0) {
-    nav.dataset.index  = 12; 
+    nav.dataset.index = 12;
     navAlt.dataset.index = 12;
   }
-  
-  for(let month of container.children) {
+
+  for (let month of container.children) {
     //console.dir(month);
-        
-    if (month.tagName === 'DIV') {
-  
+
+    if (month.tagName === "DIV") {
       if (Number(nav.dataset.index) === Number(month.id)) {
-        month.style.visibility = 'visible';
-        lastSelectedMonth = month;
-      } 
-      else { 
-        month.style.visibility = 'hidden';
+        month.style.visibility = "visible";
+        //lastSelectedMonth = month;
+      } else {
+        month.style.visibility = "hidden";
       }
     }
-  
-  };
-};
+  }
+}
 
 export default Calendar;
